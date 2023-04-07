9e338f486da42fc8ed9cca7ff012704ae506e8fe
tiagorodrisi <112359948+tiagorodrisi@users.noreply.github.com>
2023-04-06 22:11:44 -0300

    Update Lampada.java

--- Lampada.java
+++ Lampada.java
@@ -1,6 +1,9 @@
 public class Lampada extends DispositivoInteligente {
+    private int intensidade;
+    
     public Lampada(String dispositivo) {
         super(dispositivo);
+        this.intensidade = 50;
     }
     
     public void ligar() {
@@ -11,5 +14,14 @@ public class Lampada extends DispositivoInteligente {
     public void desligar() {
         setEstado(false);
         System.out.println(getDispositivo() + " desligada.");
+    }
+    
+    public int getIntensidade() {
+        return intensidade;
+    }
+    
+    public void setIntensidade(int intensidade) {
+        this.intensidade = intensidade;
+        System.out.println("A intensidade da " + getDispositivo() + " agora é " + intensidade + "%.");
     }
 }
